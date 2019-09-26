d880f4f08f723363abb29aab00e9ad4f7b5fb638
lilac <zhangguochao>
2019-09-26 16:02:51 +0800

    AC 669 看了题解AC的

---
+++ src/P_669_修剪二叉搜索树/Solution.java
@@ -0,0 +1,49 @@
+package P_669_修剪二叉搜索树;
+
+public class Solution {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+
+
+        if (root == null){
+            return null;
+        }
+
+        if (root.val < L){
+            return trimBST(root.left, L, R);
+        }else if (R < root.val){
+            return trimBST(root.right, L, R);
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+
+        node3.left = node0;
+        node3.right = node4;
+        node0.right = node2;
+        node2.left = node1;
+        System.out.println(new Solution().trimBST(node3, 1, 3));
+
+    }
+}
