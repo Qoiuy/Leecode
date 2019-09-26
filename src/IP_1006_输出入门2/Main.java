51da9e6a35c7b781e0dc882f8931d867bb605434
lilac <zhangguochao>
2019-09-26 16:39:49 +0800

    AC imust 1006

---
+++ src/IP_1006_输出入门2/Main.java
@@ -0,0 +1,25 @@
+package IP_1006_输出入门2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+        int length = cin.nextInt();
+
+        for (int i = 0; i < length; i++){
+            int a, b;
+
+            a = cin.nextInt();
+            b = cin.nextInt();
+
+            System.out.println((a + b) );
+        }
+
+
+
+    }
+
+}
