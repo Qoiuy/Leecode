919060de2aff63d79b7eee82195d6927be2d4959
lilac <zhangguochao>
2019-09-26 18:01:22 +0800

    AC imust 1007 牛b 牛b因为一个float类型的PI 让我AC不成

---
+++ src/IP_1007_计算球体积/Main.java
@@ -0,0 +1,26 @@
+package IP_1007_计算球体积;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+
+        while (cin.hasNext()) {
+            double d = cin.nextDouble();
+            System.out.println(new DecimalFormat("0.000").format(calculateBall(d)));
+        }
+
+    }
+
+    private static Double calculateBall(double r) {
+        double aDouble = 3.1415927F;
+        return 4 * aDouble * r * r * r / 3.0;
+    }
+
+
+}
