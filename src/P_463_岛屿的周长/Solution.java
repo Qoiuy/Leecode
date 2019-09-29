4680c71a358466c8a442087b75ad86bc62453b1d
lilac <zhangguochao>
2019-09-29 15:28:34 +0800

    AC 463

--- src/P_463_岛屿的周长/Solution.java
+++ src/P_463_岛屿的周长/Solution.java
@@ -1,67 +1,36 @@
 package P_463_岛屿的周长;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
 public class Solution {
 
     public int islandPerimeter(int[][] grid) {
 
-        Set<Grid> grids = new HashSet<>();
-
-        for (int i = 0; i < grid.length; i++){
-            for (int j =0; j < grid[i].length; j++){
-                if (grid[i][j] == 1){
-                    grids.add(new Grid(i, j));
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    count += 4;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+                        count--;
+                    }
+                    if (i + 1 < grid.length && grid[i + 1][j] == 1) {
+                        count--;
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+                        count--;
+                    }
+                    if (j + 1 < grid[i].length && grid[i][j + 1] == 1) {
+                        count--;
+                    }
                 }
             }
         }
 
-        if (grids.size() == 0){
-            return  0;
-        }
-
-        int count = grids.size() * 4;
-
-        for (Grid tmp : grids){
-            if (grids.contains(new Grid(tmp.x + 1, tmp.y))){
-                count -- ;
-            }
-            if (grids.contains(new Grid(tmp.x - 1, tmp.y))){
-                count -- ;
-            }if (grids.contains(new Grid(tmp.x, tmp.y + 1))){
-                count -- ;
-            }if (grids.contains(new Grid(tmp.x, tmp.y - 1))){
-                count -- ;
-            }
-
-        }
         return count;
     }
 
 
-    class Grid{
-        int x;
-        int y;
-
-        public Grid(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Grid grid = (Grid) o;
-            return x == grid.x &&
-                    y == grid.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+    public static void main(String[] args) {
+        System.out.println(        new Solution().islandPerimeter(new int[][]{{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}}));
     }
+
 }
