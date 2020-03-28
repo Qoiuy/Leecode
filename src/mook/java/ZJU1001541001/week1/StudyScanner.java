f8e97de042612b7fb13695419e41d16abbc98a1f
lilac <zhangguochao>
2020-03-28 23:48:27 +0800

    mooc 编程 教老弟的代码

---
+++ src/mook/java/ZJU1001541001/week1/StudyScanner.java
@@ -0,0 +1,20 @@
+package mook.java.ZJU1001541001.week1;
+
+import java.util.Scanner;
+
+public class StudyScanner {
+
+    public static void main(String[] args) {
+
+        //假设 史瑞鑫 能读取键盘输入
+        // 我让史瑞鑫 读取一个 浮点数
+
+        //先给 史瑞鑫 赋予 读键盘的功能 。谁能给别人赋予读键盘的功能呢 scanner
+
+        Scanner 能读取键盘输入的史瑞鑫 = new Scanner(System.in);
+        float 史瑞鑫读取到的浮点数 = 能读取键盘输入的史瑞鑫.nextFloat();
+        System.out.println(史瑞鑫读取到的浮点数);
+
+
+    }
+}
